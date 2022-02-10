0c25ed95dd69dd7d1d78479f39dc040d2de3d8e0
weijiecode <245619254@qq.com>
2022-02-11 01:14:28 +0800

    test

--- routers/test.js
+++ routers/test.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const tokenK = require('../middleware/tokenKey')
-//测试
+// 测试
 router.post('/test',tokenK,require('../controller/test').testcontroller)
 module.exports = router
