2f92a2bf08c21e472b7c45faf803f0075ce52200
weijiecode <245619254@qq.com>
2022-02-15 03:40:30 +0800

    change and delete

--- controller/backlog.js
+++ controller/backlog.js
@@ -5,15 +5,17 @@ const moment = require('moment')
 class Backlog {
     // 新增待办事项
     async createbacklog(request, resposne, next) {
-        let insertSql = 'insert into backlog(`username`,`contents`,`datetime`,`class`,`createtime`)values(?,?,?,?,?)'
+        let insertSql = 'insert into backlog(`username`,`contents`,`datetime`,`classvalue`,`colorbg`,`createtime`)values(?,?,?,?,?,?)'
         let params = [
             request.username,
             request.body.contents,
             request.body.datetime,
-            request.body.class,
+            request.body.classvalue,
+            request.body.colorbg,
             moment().format('YYYY-MM-DD HH:mm:ss')
         ]
-        // console.log(params)
+        console.log(params)
+        console.log(insertSql)
         try{
             let result = await db.exec(insertSql,params)
             if(result && result.affectedRows >= 1){
@@ -64,7 +66,7 @@ class Backlog {
             })
         }
     }
-    // 修改待办事项完成状态
+    // 修改是否已完成待办事项
     async updatedone(request, resposne, next){
         let updateSql = 'update backlog set done=? where username=? and id=?'
         let params = [
@@ -74,7 +76,7 @@ class Backlog {
         ]
         try{
             let result = await db.exec(updateSql,params)
-            if(result && result.affectedRows){
+            if(result && result.affectedRows >= 1){
                 resposne.json({
                     code: 200,
                     msg: '修改成功'
@@ -83,6 +85,66 @@ class Backlog {
                 resposne.json({
                     code: 201,
                     msg: '修改失败，请重试'
+                })
+            }
+        }catch(error){
+            resposne.json({
+                code: -201,
+                msg: '服务器异常',
+                error
+            })
+        }
+    }
+    // 修改待办事项内容
+    async updatecontents(request, resposne, next){
+        let updateSql = 'update backlog set contents=?, datetime=?,classvalue=?,colorbg=? where username=? and id=?'
+        let params = [
+            request.body.contents,
+            request.body.datetime,
+            request.body.classvalue,
+            request.body.colorbg,
+            request.username,
+            request.body.id
+        ]
+        try{
+            let result = await db.exec(updateSql,params)
+            if(result && result.affectedRows >= 1){
+                resposne.json({
+                    code: 200,
+                    msg: '修改待办事项成功'
+                })
+            }else{
+                resposne.json({
+                    code: 201,
+                    msg: '修改待办事项失败，请重试'
+                })
+            }
+        }catch(error){
+            resposne.json({
+                code: -201,
+                msg: '服务器异常',
+                error
+            })
+        }
+    }
+    // 删除待办事项
+    async delebacklog(request, resposne, next){
+        let deleteSql = 'delete from backlog where username=? and id=?'
+        let params = [
+            request.username,
+            request.body.id
+        ]
+        try{
+            let result = await db.exec(deleteSql,params)
+            if(result && result.affectedRows >= 1){
+                resposne.json({
+                    code: 200,
+                    msg: '删除成功'
+                })
+            }else{
+                resposne.json({
+                    code: 201,
+                    msg: '删除失败，请重试'
                 })
             }
         }catch(error){
--- routers/backlog.js
+++ routers/backlog.js
@@ -5,5 +5,7 @@ const tokenK = require('../middleware/tokenKey')
 router.post('/insertbacklog',tokenK,require('../controller/backlog').createbacklog)
 router.post('/selectbacklog',tokenK,require('../controller/backlog').selectbacklog)
 router.post('/updatebacklog',tokenK,require('../controller/backlog').updatedone)
+router.post('/updatecontents',tokenK,require('../controller/backlog').updatecontents)
+router.post('/deletebacklog',tokenK,require('../controller/backlog').delebacklog)
 
 module.exports = router
